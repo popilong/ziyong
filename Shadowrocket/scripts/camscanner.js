1c7c45e2006b606c4db8a7b7506dc1abdb8343c7
smartxiaoeba <3231012@gmail.com>
2022-11-02 14:34:44 +0800

    Update camscanner.js

--- Shadowrocket/scripts/camscanner.js
+++ Shadowrocket/scripts/camscanner.js
@@ -1,3 +1,3 @@
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1687017600"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"9915126887"}}};
 $done({body: JSON.stringify(obj)});
