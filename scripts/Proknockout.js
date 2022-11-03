df632fcd6908b9136194a98f0c56339190cbecc4
smartxiaoeba <3231012@gmail.com>
2022-11-04 07:09:56 +0800

    Update Proknockout.js

--- scripts/Proknockout.js
+++ scripts/Proknockout.js
@@ -1,29 +1,3 @@
-/******************************
-
-脚本功能：Proknockout扣图神器+解锁VIP
-下载地址：http://mtw.so/6rugdA
-软件版本：7.12
-脚本作者：彭于晏💞
-更新时间：2022-11-4
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
-*******************************
-
-[rewrite_local]
-
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Proknockout.js
-
-[mitm] 
-
-hostname = buy.itunes.apple.com
-
-*******************************/
-
-
 var objc = JSON.parse($response.body);
 
     objc = 
